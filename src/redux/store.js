0531b9dcf8c7eafabadc109be0e59a866b9e131a
Probir Sarkar <probirsarkar855@gmail.com>
2023-04-13 08:15:49 +0530

    persist-data

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,22 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import riskReducer from "./risk/riskSlice";
 
-export const store = configureStore({
-  reducer: {
-    risk: riskReducer,
-  },
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const rootReducer = combineReducers({
+  risk: riskReducer,
 });
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
